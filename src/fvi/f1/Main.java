8ac22ce5f45402a7ff1aab83aa9520d0a4ace84d
Victoria Frantsuh <eboko1@gmail.com>
2016-10-29 21:58:08 +0300

    add repaint

--- src/fvi/f1/Main.java
+++ src/fvi/f1/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main (String[] args){
         JFrame f= new JFrame("Java F1");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(500,400);
+        f.setSize(1200,700);
         f.add(new Road() );
         f.setVisible(true);
 
